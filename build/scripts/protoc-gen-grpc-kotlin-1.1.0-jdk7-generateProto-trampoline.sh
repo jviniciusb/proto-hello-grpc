890e0b95f991d01e53ebd640271f9d50eb16b3d5
jviniciusb <jaderviniciusb@gmail.com>
2021-09-11 16:52:01 -0300

    Added publishing block inside gradle

--- build/scripts/protoc-gen-grpc-kotlin-1.1.0-jdk7-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/opt/android-studio/jre/bin/java' -jar '/home/jader/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.1.0/bd2055d9d381f0dc2cdbf0c5a7b71493bb1ce8f9/protoc-gen-grpc-kotlin-1.1.0-jdk7.jar' "$@"
