83130e7d37bd0950da7a95a61146a7099e1974c4
jviniciusb <jaderviniciusb@gmail.com>
2021-09-11 17:06:33 -0300

    Updated publishing block

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,8 @@ publishing {
             groupId = project.group.toString()
             artifactId = project.name
             version = project.version.toString()
+
+            from(components["java"])
         }
     }
 }
